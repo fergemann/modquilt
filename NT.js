e2db6f6ec7ff1bf3c184ecb71f318822f8aeb041
Dave Fergemann <fergemann@gmail.com>
2014-05-08 16:57:00 -0400

    Clean up problems found by jshint

--- NT.js
+++ NT.js
@@ -5,13 +5,13 @@ var NT = {
 
     // finds a solution over integers to ax + by = gcd(a,b). returns g, x, y.
     lineq: function(a,b) {
-        if (b == 0) return {g:a, x:1, y:0};
+        if (b === 0) { return {g:a, x:1, y:0}; }
         var x = 1;
         var g = a;
         var v = 0;
         var w = b;
         var q, r, s;
-        while (w != 0) {
+        while (w !== 0) {
             q = Math.floor(g/w);
             r = NT.mod(g,w);
             s = x - q*v;
@@ -20,38 +20,38 @@ var NT = {
             v = s;
             w = r;
         }
-        var y = (g-a*x)/b
+        var y = (g - a*x)/b;
         if (g < 0) {
             g = -g;
             x = -x;
             y = -y;
         }
-        return {g:g, x:x, y:y}
+        return {g:g, x:x, y:y};
     },
     
     // modular inverse
     inv: function(a, m) {
-        eq = NT.lineq(a, m);
-        if (eq.g != 1) {
+        var eq = NT.lineq(a, m);
+        if (eq.g !== 1) {
             return NaN;
         }
         return NT.mod(eq.x, m);
     },
     
     powMod: function(base, exp, m) {
-        if (exp == 0 && base == 0) return NaN;
-        if (exp == 0) return 1;
-        if (exp > 0 && base == 0) return 0;
+        if (exp === 0 && base === 0) { return NaN; }
+        if (exp === 0) { return 1; }
+        if (exp > 0 && base === 0) { return 0; }
         if (exp < 0) { 
             var i = NT.inv(base,m);
-            if (i == NaN) return NaN;
+            if (isNaN(i)) { return NaN; }
             return NT.powMod(i, -exp, m);
         }
-        if (exp % 2 == 0) {
+        if (exp % 2 === 0) {
             return NT.mod(Math.pow(NT.powMod(base, exp/2, m), 2),m);
         } else {
             return NT.mod((base * NT.powMod(base, exp-1, m)),m);
-    }
-}   
-
-}
+        }
+    }   
+    
+};
--- modquilt.js
+++ modquilt.js
@@ -1,58 +1,53 @@
+/* global document, window, d3, NT */
+/* exported chooseOperator, trigger */
 var min = 2;
 var max = 11;
-var colors = ['#000000','#ffffff','#000060','#ff6657','#00800f','#dd7500','#1000b0','#7020a0','#c00020','#b040a0','#ddcc00'];
+var colors = ['#000000','#ffffff','#000060','#ff6657','#00800f','#dd7500',
+              '#1000b0','#7020a0','#c00020','#b040a0','#ddcc00'];
 colors[NaN] = '#808080';
 
-var invcolors = ['#ffffff','#000000','#ffffbf','#0088a8','#ff7ff0','#009aff','#ffff7f','#4f2fcf','#2fbfdf','#4fbf5f','#000000'];
-
-var m = 11;
+var m = 10;
 var gridSize = 46;  // approximate number of squares across and down
 var transitionTime = 500;
 var rows = 0;
 var operators = {
-    "+": function(a, b, m) { return NT.mod(a+b, m) },
-    "*": function(a, b, m) { return NT.mod(a*b, m) },
-    "^": function(a, b, m) { return NT.powMod(a, b, m) }
-}
+    "+": function(a, b, m) { return NT.mod(a+b, m); },
+    "*": function(a, b, m) { return NT.mod(a*b, m); },
+    "^": function(a, b, m) { return NT.powMod(a, b, m); }
+};
 
 var operator = "*";
 
 var squareDivHtml = function(a, b, m) {
     switch(operator) {
         case "+":
-            return a + " + " + b + " &equiv; " + operators["+"](a,b,m) + " mod " + m;
+            return a + " + " + b + " &equiv; " + 
+                   operators["+"](a,b,m) + " mod " + m;
         case "*":
             return a + " &times; " + b + " &equiv; " + 
                    operators["*"](a,b,m) + " mod " + m;
         case "^":
             var c = operators["^"](a,b,m);
-            var aParen = (a < 0) ? '('+a+')' : a
+            var aParen = (a < 0) ? '('+a+')' : a;
             if (isNaN(c)) {
                 return aParen + '<sup>' + b  + '</sup> is undefined mod ' + m;
             } else {
-                return aParen + '<sup>' + b + '</sup> &equiv; ' + c + " mod " + m;
+                return aParen + '<sup>' + b + '</sup> &equiv; ' + 
+                       c + " mod " + m;
             }
     }
-}
-
-var newshadow = function(c,pix,blur) {
-    var ret = '';
-    for (var i = -pix; i <= pix; i+= pix) {
-        for (var j = -pix; j <= pix; j+= pix) {
-            ret += c + " " + i + "px " + j + "px " + blur + "px, ";
-        }
-    }
-    return ret + "0px 0px";
-}
+};
 
 var chooseOperator = function(op) {
     operator = op;
     trigger(0);
-}
-    
+};
+
 var trigger = function(off) {
     d3.selectAll(".selectedOperator").classed("selectedOperator", false);
-    buttonIds = {"+":"#plusButton", "*":"#timesButton", "^":"#powerButton"}
+    var buttonIds = {"+":"#plusButton", 
+                     "*":"#timesButton", 
+                     "^":"#powerButton"};
     d3.select(buttonIds[operator]).classed("selectedOperator", true);
     var oldRows = rows;
     m = (m + off - min)%((max-min)+1) + min;
@@ -64,12 +59,40 @@ var trigger = function(off) {
     d3.select("#quilt")
         .transition()
         .duration(transitionTime)
-        .attr("viewBox", -(gridSize+1)/2 + " " + -(gridSize+1)/2 
-                          + " " + (gridSize+1) + " " + (gridSize+1));
+        .attr("viewBox", -(gridSize+1)/2 + " " + -(gridSize+1)/2 +
+                         " " + (gridSize+1) + " " + (gridSize+1));
     var infoDiv = d3.select("#squareInfo");
     
+    var getXCoord = function(d) { return d.x - 0.5; };
+    var getYCoord = function(d) { return d.y - 0.5; };
+    var getColor = function(d) { 
+        return colors[operators[operator](d.x, d.y, m)]; 
+    };
+    var mouseOverHandler =  function (d) {
+        //prevent info div from falling off page
+        var left = d3.event.pageX;
+        if ((left + infoDiv[0][0].clientWidth) > window.innerWidth) {
+            left = left - infoDiv[0][0].clientWidth;
+        }
+        infoDiv.transition()
+            .delay(500)
+            .duration(200)
+            .style("opacity", 0.9)
+            .style("left", left + "px")
+            .style("top", (d3.event.pageY - infoDiv[0][0].clientHeight) + "px")
+            .each("start", function() {
+                infoDiv.html(squareDivHtml(d.x, d.y, m));
+            });
+    };
+    var mouseOutHandler = function () {
+        infoDiv.transition()
+            .duration(400)
+            .style("opacity", 0);
+    };
+
+    
     for (var y = 0;  y < Math.max(rows, oldRows); y++) {
-        data = []
+        var data = [];
         if (y < rows) {
             for (var x = 0; x < rows; x++) {
                 data.push({x:x, y:y});
@@ -84,7 +107,7 @@ var trigger = function(off) {
                 }
             }
         }
-        rects = d3.select("#quilt g").selectAll("rect.row"+y)
+        var rects = d3.select("#quilt g").selectAll("rect.row"+y)
             .data(data);
         rects.enter()
             .append("rect")
@@ -94,31 +117,14 @@ var trigger = function(off) {
             .style("opacity",0);
         rects.transition()
             .duration(transitionTime)
-            .attr("x",function(d) { return d.x-.5; })
-            .attr("y", function(d) { return d.y-.5; })
+            .attr("x", getXCoord)
+            .attr("y", getYCoord)
             .attr("width", 1)
             .attr("height", 1)
             .style("opacity",1)
-            .style("fill", function(d) { return colors[operators[operator](d.x, d.y, m)] });
-        rects.on("mouseover", function (d) {
-                //prevent info div from falling off page
-                var left = d3.event.pageX;
-                if ((left + infoDiv[0][0].clientWidth) > window.innerWidth) {
-                    left = left - infoDiv[0][0].clientWidth;
-                }
-                infoDiv.transition()
-                    .delay(500)
-                    .duration(200)
-                    .style("opacity", .9)
-                    .style("left", left + "px")
-                    .style("top", (d3.event.pageY - infoDiv[0][0].clientHeight) + "px")
-                    .each("start", function() {infoDiv.html(squareDivHtml(d.x, d.y, m))});
-            })
-            .on("mouseout", function (d) {
-                infoDiv.transition()
-                    .duration(400)
-                    .style("opacity", 0)
-            });  
+            .style("fill", getColor);
+        rects.on("mouseover", mouseOverHandler)        
+            .on("mouseout", mouseOutHandler);  
         rects.exit()
             .transition()
             .duration(transitionTime)
@@ -132,8 +138,4 @@ var trigger = function(off) {
     
     var modtext = document.getElementById("modulus");
     modtext.innerHTML = m;
-}
-    
-document.onkeypress = function(k) {
-
-}
+};
